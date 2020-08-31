62f928261586f2e9c2704b8903b4700bced1a3ed
Dmytro <34006606+Dimonium-239@users.noreply.github.com>
2020-08-31 19:29:00 +0200

    Add files via upload

---
+++ src/main/java/com/gunghorse/horsehome/HorseHomeApplication.java
@@ -0,0 +1,13 @@
+package com.gunghorse.horsehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorseHomeApplication.class, args);
+    }
+
+}
